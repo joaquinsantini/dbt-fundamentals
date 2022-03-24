086d808bc760eb3ddbc320b3a027e409c9782025
Joaquin Santini <joaquinsantini@hotmail.com>
2022-03-24 15:09:39 +0000

    Finish dbt fundamentals course

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,7 @@
+select  id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        amount / 100 as amount,
+        created,
+        status
+        from {{ source('stripe', 'payment') }}
